8348e7d450f7932b79688793062b7de2e4ebd9db
danu999 <danusaputraoffice@gmail.com>
2023-05-17 15:46:08 +0700

    Update

--- src/controller/users.js
+++ src/controller/users.js
@@ -19,11 +19,18 @@ const getAllUsers = async (req, res) => {
 //Create New User
 const createNewUser = async (req, res) => {
   const { body } = req;
+  if (!body.email || !body.name || !body.addres) {
+    return res.status(400).json({
+      message: "Anda mengirimkan data yang salah",
+      data: null,
+    });
+  }
+
   try {
     await UsersModel.createNewUser(body);
-    res.json({
-      message: "Create New User Successfully",
-      data: body
+    res.status(201).json({
+      message: "CREATE new user success",
+      data: body,
     });
   } catch (error) {
     res.status(500).json({
@@ -34,25 +41,41 @@ const createNewUser = async (req, res) => {
 };
 
 //Update User
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   const { id } = req.params;
-  console.log("Id: ", id);
-  res.json({
-    message: "Update User Successfully",
-    data: req.body,
-  });
+  const { body } = req;
+  try {
+    await UsersModel.updateUser(body, id);
+    res.json({
+      message: "UPDATE user success",
+      data: {
+        id: id,
+        ...body,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      serverMessage: error,
+    });
+  }
 };
 
 //Delete User
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: "Delete User Successfully",
-    data: id,
-    name: "Danu",
-    email: "danu@gmail.com",
-    address: "Tangerang Selatan",
-  });
+  try {
+    await UsersModel.deleteUser(id);
+    res.json({
+      message: "DELETE user success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      serverMessage: error,
+    });
+  }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 const usersRoutes = require("./routes/users.js");
 const datamasterRoutes = require("./routes/datamaster.js");
 const middlewareLogRequest = require("./middleware/logs.js");
-require("dotenv").config();
+const upload = require('./middleware/multer');
+
 
 const app = express();
 
@@ -13,83 +15,23 @@ app.use(middlewareLogRequest);
 app.use(express.json());
 app.use("/users", usersRoutes);
 app.use("/datamaster", datamasterRoutes);
+app.use('/assets', express.static('public/images'))
+
+app.post('/upload',upload.single('photo'),(req, res) => {
+  res.json({
+      message: 'Upload berhasil'
+  })
+})
+
+app.use((err, req, res, next) => {
+  res.json({
+      message: err.message
+  })
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
 
-// // get a single data by id
-// app.get('/data/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const result = data.find(item => item.id === id);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.status(404).json({ message: 'Data not found' });
-//   }
-// });
-
-// // create new data
-// app.post('/data', (req, res) => {
-//   const { name, age, email } = req.body;
-//   const id = data.length + 1;
-//   data.push({ id, name, age, email });
-//   res.status(201).json({ message: 'Data created successfully' });
-// });
-
-// // update a data by id
-// app.put('/data/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const { name, age, email } = req.body;
-//   const index = data.findIndex(item => item.id === id);
-//   if (index !== -1) {
-//     data[index] = { id, name, age, email };
-//     res.json({ message: 'Data updated successfully' });
-//   } else {
-//     res.status(404).json({ message: 'Data not found' });
-//   }
-// });
-
-// // delete a data by id
-// app.delete('/data/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const index = data.findIndex(item => item.id === id);
-//   if (index !== -1) {
-//     data.splice(index, 1);
-//     res.json({ message: 'Data deleted successfully' });
-//   } else {
-//     res.status(404).json({ message: 'Data not found' });
-//   }
-// });
-
-// const express = require("express");
-// const app = express();
-// const port = 3005;
-// const bodyParser = require("body-parser");
-
-// app.use(bodyParser.json());
-
-// app.get("/", (request, response) => {
-//   response.send("Halaman Utama Guys");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// app.post("/login", (req, res) => {
-//   console.log({ requestFromOutside: req.body });
-//   res.send("login berhasil");
-// });
-
-// app.put("/username", (req, res) => {
-//   console.log({ Updatedata: req.body });
-//   res.send("Update Berhasil");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
